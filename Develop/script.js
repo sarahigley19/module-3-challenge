ef08505d0bd187abd1b8177ed1af028ed64c2e79
Sarah Higley <sarah.higley19@gmail.com>
2024-01-15 12:27:38 -0700

    some progres, maybe?

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,45 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+var writePassword = {
+  lowerCase: randomLowerCase,
+  upperCase: randomUpperCase,
+  spCharSet: randomSpCharSet,
+  numbers: randomNumbers
+}
+generateBtn.addEventListener("click", writePassword); {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function writePassword(lowerCase, upperCase, spCharSet, numbers) {
+
+}
+
+//Password Variables
+var lowerCase = confrim("Please have at least one lowercase character.");
+var upperCase = confirm("Please have at least one uppercase character.");
+var numbers = cofirm("Please have at least one number.");
+var spCharSet = confirm("Please have at least one speical character.");
+var charLength = prompt("Please create a password between 8 and 128 characters long.");
+
+//Password Qulaifications
+function randomLowerCase() {
+  var alphabetLow = 'abcdefghijklmnopqrstuvwxyz';
+  return alphabetLow[Math.floor(Math.random() * alphabetLow.length)];
+}
+
+function randomUpperCase() {
+  var alphabetUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return alphabetUpper[Math.floor(Math.random() * alphabetUpper.length)];
+}
+
+function randomSpCharSet() {
+  var randomSpCharset = '!@#$%^&*()<>?/.';
+  return randomSpCharSet[Math.floor(Math.random() * randomSpCharSet.length)];
+}
+
+function randomNumbers() {
+  var randomNumbers = '0123456789';
+  return randomNumbers[Math.floor(Math.random() * randomNumbers.length)];
+}
+
