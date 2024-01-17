8e723c52138f3cd704b7bba90c650c325005f342
Sarah Higley <sarah.higley19@gmail.com>
2024-01-16 21:57:26 -0700

    final changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
-var allVariables = []
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var spCharSet = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')','?','/'];
-var minCharLength = 8;
-var maxCharLength = 128;
+//Password Variables
+var charVar = {
+  lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  spCharSet: '!@#$%^&*()?/',
+};
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -16,45 +16,48 @@ generateBtn.addEventListener("click", writePassword);
 function writePassword() {
   var correct = passwordQualifers();
   if (correct) {
-    var newPassword = generatePassword();
+    var unlockNewPassword = generatePassword();
     var passwordText = document.querySelector("#password");
 
-    passwordText.value = newPassword;
+    passwordText.value = unlockNewPassword;
   }
 }
 
 function generatePassword() {
-  var password = "";
-  for (var i = 0; i > allVariables; i++) {
-    var randomIndex = (Math.floor(Math.random() * passwordQualifers.length));
-    password = newpassword + passwordQualifers[randomIndex];
-    
+  let password = "";
+  const charSet = Object.values(charVar).join('');
+
+  for (let i = 0; i < charLength; i++) {
+    const randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
   }
+
   return password;
 }
 
-function passwordQualifers() {
-  minCharLength = parseInt(prompt("How many Characters would you like your password to be? (Please select a number between 8 and 128."));
 
-  if (minCharLength <= 8 && maxCharLength >= 128) {
+function passwordQualifers() {
+  charLength = parseInt(prompt("How many Characters would you like your password to be? (Please select a number between 8 and 128."));
+
+  if (charLength < 8 || charLength > 128) {
     alert("Please create a password that is more than 8 characters long and less than 128 characters long.");
     return false;
   }
 
-  if (confirm("Would you like to least one lowercase letter?")); {
-    allVariables = allVariables.concat(lowerCase);
+  if (confirm("Would you like to include at least one lowercase letter?")) {
+    charVar.lowerCase = charVar.lowerCase;
   }
-  if (confirm("Would you like to include at least one uppercase letter?")); {
-    allVariables = allVariables.concat(upperCase);
+  if (confirm("Would you like to include at least one uppercase letter?")) {
+    charVar.upperCase = charVar.upperCase;
   }
-  if (confirm("Would you like this to include at least one number?")); {
-    allVariables = allVariables.concat(numbers);
+  if (confirm("Would you like this to include at least one number?")) {
+    charVar.numbers = charVar.numbers;
   }
-  if (confirm("Would you like this to include at least one special character?")); {
-    allVariables = allVariables.concat(spCharSet);
+  if (confirm("Would you like this to include at least one special character?")) {
+    charVar.spCharSet = charVar.spCharSet;
   }
   return true;
-
 }
+
 
 
